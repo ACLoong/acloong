13ddaa759793c35dd2a99f3fa7c5e9cffd44cff9
WangQing <kingchin1218@126.com>
2018-01-08 23:45:14 +0800

    adjust name format
    `

---
+++ src/core/string.cpp
@@ -0,0 +1,17 @@
+//
+// Created by WangQing on 04/01/2018.
+//
+
+#include "core/string.h"
+
+namespace ac {
+    Character::Character(char character)
+    {
+        //TODO
+    }
+
+    Character::Character(const Character &character)
+    {
+        //TODO
+    }
+}
---
+++ src/core/uncopyable.hpp
@@ -0,0 +1,24 @@
+//
+// Created by WangQing on 2018/1/6.
+//
+
+#ifndef ACLOONG_UNCOPYABLE_HPP
+#define ACLOONG_UNCOPYABLE_HPP
+
+namespace ac {
+    /**
+     * @brief
+     */
+    class Uncopyable {
+    protected:
+        Uncopyable () = default;
+        virtual ~ACUncopyable () = default;
+
+        Uncopyable (const Uncopyable&) = delete;
+        Uncopyable& operator= (const Uncopyable&) = delete;
+    };
+}
+
+
+
+#endif //ACLOONG_UNCOPYABLE_HPP
